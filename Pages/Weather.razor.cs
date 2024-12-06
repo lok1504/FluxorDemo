5ceaf4049144369b728118f989b08c5b629ec71e
Yap Joon Lok <jl.yap@klk.com.my>
2024-12-06 17:33:41 +0800

    Refactor codes

---
+++ Pages/Weather.razor.cs
@@ -0,0 +1,14 @@
+﻿using FluxorDemo.Models;
+using System.Net.Http.Json;
+
+namespace FluxorDemo.Pages;
+
+public partial class Weather
+{
+    private WeatherForecast[] forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+    }
+}
